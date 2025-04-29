000c97e97dd95a6e57c92ea6c9069f3eb65afaa9
thanavreddy <thanavreddy196@gmail.com>
2025-04-29 19:54:23 +0530

    31

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-import React, { useEffect, useState } from 'react';
-
-function LifecycleDemo() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log('Component Mounted');
-    return () => console.log('Component Unmounted');
-  }, []);
-
-  useEffect(() => {
-    console.log('Component Updated');
-  }, [count]);
-
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
-  );
-}
+function Counter() {
+    const [count, setCount] = useState(0);
+    return (
+      <div>
+        <h2>{count}</h2>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+    );
+  }
